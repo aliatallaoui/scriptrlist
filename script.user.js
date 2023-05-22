96ba74392066c0057258704ff210d4c6fd34eb09
Hachadi Younes <neobuxyou@gmail.com>
2023-05-22 02:19:09 +0100

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -17,18 +17,6 @@
     'use strict';
     let token ='123456';
     localStorage.setItem('access_token',token);
-
-  /*   async function fetchToken(token) {
-        const response = await fetch("https://api-v1.riskblacklist.com/api/check-token?token="+token);
-        const result = await response.json();
-      return result.valid
-      }
-                                                 |/|/|/|             I M P O R T A N E T               |/|/|/|
-     validToken= fetchToken(token);
-      
-if(validToken) */
-
-
     if(window.location.href.includes("/dane/"))
     {
         if(!window.top.location.href.includes("#install")){
@@ -36,13 +24,10 @@ if(validToken) */
         }
     }
     document.addEventListener("DOMContentLoaded", async function() {
-        //import css
         const link = document.createElement('link');
         link.rel = 'stylesheet';
         link.href = 'https://login.riskblacklist.com/wp-content/themes/blist/scriptrlist/custom.css';
         document.head.appendChild(link);
-        //end import css
-
         const excludeKeywords = ['facebook', 'youtube', 'google', 'riskblacklist','trueclient'];
         const currentUrl = window.location.href;
         if (excludeKeywords.some(keyword => currentUrl.includes(keyword))) {
@@ -52,7 +37,6 @@ if(validToken) */
         const hostName=window.location.hostname;
         const publicScript_Url = API_URL+ "get-script?url=" +hostName+"/public";
         const privateScript_Url = API_URL+ "get-script?url=" +hostName +"/private";
-        // Fetch the URL of the userscript
         const response = await fetch(publicScript_Url, {
             headers: {
                 'Accept': 'application/json',
@@ -60,19 +44,13 @@ if(validToken) */
                 "X-Api-Key": "3AQi9ysOlvV6ZsYyl5OiUe31ckl6XVndBcMRFHeY"
             }
         });
-
-        // Load the userscript from the URL returned by the API
         const firstScriptURL = await response.json();
         const scriptPublic = document.createElement('script');
         scriptPublic.setAttribute('src', firstScriptURL.scriptPath);
-
         const vDataBase = document.createElement('script');
         vDataBase.textContent = `let versionDataBase = ${firstScriptURL.versions};`;
         document.body.append(vDataBase);
-
-
         scriptPublic.addEventListener('load', async function() {
-
             const response = await fetch(privateScript_Url, {
                 headers: {
                     'Accept': 'application/json',
@@ -80,17 +58,11 @@ if(validToken) */
                     "X-Api-Key": "3AQi9ysOlvV6ZsYyl5OiUe31ckl6XVndBcMRFHeY"
                 }
             });
-
-            // Load the userscript from the URL returned by the API
             const secondScriptURL = await response.json();
             const scriptPrivate = document.createElement('script');
             scriptPrivate.setAttribute('src', secondScriptURL.scriptPath);
             document.head.appendChild(scriptPrivate);
-
         });
-
         document.head.appendChild(scriptPublic);
-
 });
-
 })();
